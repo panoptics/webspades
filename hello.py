54a0314357fd184c4cbf08e5a605ceb231855583
len <panopticon.spoon.digger@gmail.com>
2014-04-18 20:29:11 +0100

    twisted

--- hello.py
+++ hello.py
@@ -1,10 +1,17 @@
 import os
-from flask import Flask
 
-app = Flask(__name__)
+from twisted.web import server, resource
+from twisted.internet import reactor
 
-@app.route('/')
-def hello():
-    return 'Hello World!'
+class HelloResource(resource.Resource):
+    isLeaf = True
+    numberRequests = 0
+    
+    def render_GET(self, request):
+        self.numberRequests += 1
+        request.setHeader("content-type", "text/plain")
+        return "I am request #" + str(self.numberRequests) + "\n"
 
+reactor.listenTCP(8080, server.Site(HelloResource()))
+reactor.run()
 
