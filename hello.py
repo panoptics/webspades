2c600156a7476daac831730ccb516c4597303ac2
len <panopticon.spoon.digger@gmail.com>
2014-04-18 20:51:30 +0100

    sd

--- hello.py
+++ hello.py
@@ -1,7 +1,17 @@
 import os
-
 from twisted.web import server, resource
 from twisted.internet import reactor
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route('/')
+def hello():
+    return 'Hello World!'
+
+
+
+
 
 class HelloResource(resource.Resource):
     isLeaf = True
