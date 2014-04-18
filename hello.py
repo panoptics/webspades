1a32c4521e8cb092fc0c5932fbacc449a414996c
len <panopticon.spoon.digger@gmail.com>
2014-04-18 20:53:42 +0100

    sd

--- hello.py
+++ hello.py
@@ -8,20 +8,3 @@ app = Flask(__name__)
 @app.route('/')
 def hello():
     return 'Hello World!'
-
-
-
-
-
-class HelloResource(resource.Resource):
-    isLeaf = True
-    numberRequests = 0
-    
-    def render_GET(self, request):
-        self.numberRequests += 1
-        request.setHeader("content-type", "text/plain")
-        return "I am request #" + str(self.numberRequests) + "\n"
-
-reactor.listenTCP(82, server.Site(HelloResource()))
-reactor.run()
-
