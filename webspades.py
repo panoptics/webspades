191e03ca62c34c98099e63760fbfcf2d9c49bf2b
len <panopticon.spoon.digger@gmail.com>
2014-04-19 20:59:47 +0100

    websock

---
+++ webspades.py
@@ -0,0 +1,27 @@
+from twisted.web import static, server, resource
+#from autobahn.twisted.choosereactor import install_reactor
+
+from autobahn.twisted.websocket import WebSocketServerFactory,WebSocketServerProtocol
+#reactor = install_reactor()
+#print("Running on reactor {}".format(reactor))
+
+class WebSpadesProtocol(WebSocketServerProtocol):
+    isLeaf = True
+    numberRequests = 0
+    app = 0
+    def setServiceParent(application):
+	self.app = application
+
+    def onMessage(self, payload, isBinary):
+        self.numberRequests += 1
+        ## just echo any WebSocket message received back to client
+        ##
+        self.sendMessage(str(self.numberRequests), isBinary)
+
+    def render_GET(self, request):
+        self.numberRequests += 1
+        request.setHeader("content-type", "text/plain")
+        return "I am request #" + str(self.numberRequests) + "\n"
+
+class WebSpadesServerFactory(WebSocketServerFactory):
+    protocol = WebSpadesProtocol
