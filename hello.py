858c96afce1e9f29804861229fe06932a186e7e9
len <panopticon.spoon.digger@gmail.com>
2014-04-19 13:23:55 +0100

    init mod

--- hello.py
+++ hello.py
@@ -1,12 +1,19 @@
 import os
 import gevent
 
-from flask import Flask
-from flask_sockets import Sockets
-
-app = Flask(__name__)
 PO  = os.environ['PORT']
 
-@app.route('/')
-def hello():
-    return PO
+from twisted.web import server, resource
+from twisted.internet import reactor
+
+class HelloResource(resource.Resource):
+    isLeaf = True
+    numberRequests = 0
+    
+    def render_GET(self, request):
+        self.numberRequests += 1
+        request.setHeader("content-type", "text/plain")
+        return "I am request #" + str(self.numberRequests) + "\n"
+
+reactor.listenTCP(PO, server.Site(HelloResource()))
+reactor.run()
