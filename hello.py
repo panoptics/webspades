25b36c66768ad467b4abef6d43478efb1e6e832a
len <panopticon.spoon.digger@gmail.com>
2014-04-19 13:08:59 +0100

    init mod

--- hello.py
+++ hello.py
@@ -9,3 +9,23 @@ app = Flask(__name__)
 @app.route('/')
 def hello():
     return 'Hello World!'
+
+@sockets.route('/submit')
+def inbox(ws):
+    """Receives incoming chat messages, inserts them into Redis."""
+    while ws.socket is not None:
+        # Sleep to prevent *contstant* context-switches.
+        gevent.sleep(0.1)
+        message = ws.receive()
+
+        if message:
+            app.logger.info(u'Inserting message: {}'.format(message))
+
+@sockets.route('/receive')
+def outbox(ws):
+    """Sends outgoing chat messages, via `ChatBackend`."""
+    """chats.register(ws) """
+
+    while ws.socket is not None:
+        # Context switch while `ChatBackend.start` is running in the background.
+        gevent.sleep()
