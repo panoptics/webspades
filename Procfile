5cec09513cc5ee05ad4dbefbff3fb9a6bc01fa19
len <panopticon.spoon.digger@gmail.com>
2014-04-19 13:32:07 +0100

    t

--- Procfile
+++ Procfile
@@ -1,1 +1,1 @@
-web: gunicorn hello:app
+web: bin/python hello.py
--- hello.py
+++ hello.py
@@ -1,9 +1,6 @@
 import os
-import gevent
+"""import gevent"""
 
-from flask import Flask
-
-app = Flask(__name__)
 PO  = os.environ['PORT']
 
 from twisted.web import server, resource
