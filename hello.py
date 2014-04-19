8760788f2d2f4e332c663c9bbf3bd61ce64ed528
len <panopticon.spoon.digger@gmail.com>
2014-04-19 13:14:19 +0100

    init mod

--- hello.py
+++ hello.py
@@ -10,23 +10,3 @@ PO  = os.environ['PORT']
 @app.route('/')
 def hello():
     return 'Hello World!'
-
-@sockets.route('/submit')
-def inbox(ws):
-    """Receives incoming chat messages, inserts them into Redis."""
-    while ws.socket is not None:
-        # Sleep to prevent *contstant* context-switches.
-        gevent.sleep(0.1)
-        message = ws.receive()
-
-        if message:
-            app.logger.info(u'Inserting message: {}'.format(message))
-
-@sockets.route('/receive')
-def outbox(ws):
-    """Sends outgoing chat messages, via `ChatBackend`."""
-    """chats.register(ws) """
-
-    while ws.socket is not None:
-        # Context switch while `ChatBackend.start` is running in the background.
-        gevent.sleep()
