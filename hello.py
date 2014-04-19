698a4b48c7d5ef984f9f9677b2f625228204610a
len <panopticon.spoon.digger@gmail.com>
2014-04-19 15:56:44 +0100

    p

--- hello.py
+++ hello.py
@@ -1,11 +1,23 @@
 import os
-port = os.environ['PORT']
+import logging
+import gevent
+from flask import Flask, render_template
+from flask_sockets import Sockets
 
-from flask import Flask
 
 app = Flask(__name__)
+app.debug = 'DEBUG' in os.environ
 
+from twisted.internet import reactor
+
+def aSillyBlockingMethod(x):
+    import time
+    time.sleep(2)
+    print x
 
 @app.route('/')
-def hello_world():
-    return 'Hello World!'
+def index():
+# run method in thread
+    reactor.callInThread(aSillyBlockingMethod, "2 seconds have passed")
+    reactor.run()
+    return "hi"
